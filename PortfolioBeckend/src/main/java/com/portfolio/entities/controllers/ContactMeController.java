9870f13eb88b623ede62b3f5264fba142b4ffeec
Hanan Shaikh <hannanshaikh150@gmail.com>
2024-03-26 07:20:29 +0500

    setup the controller and the rest api now its time to test it

--- PortfolioBeckend/src/main/java/com/portfolio/entities/controllers/ContactMeController.java
+++ PortfolioBeckend/src/main/java/com/portfolio/entities/controllers/ContactMeController.java
@@ -1,5 +1,32 @@
 package com.portfolio.entities.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.portfolio.entities.ContactMe;
+import com.portfolio.serviceImpl.SendMailToOwnerImpl;
+
+@RestController
 public class ContactMeController {
 
+	@Autowired
+	private SendMailToOwnerImpl sendMailToOwnerImpl;
+	
+	@PostMapping("/contact")
+	public ResponseEntity<String> contactHandler(@PathVariable ContactMe contact){
+		
+		
+		this.sendMailToOwnerImpl.sendMail(contact);
+		return new ResponseEntity<String>("Mail Sends Successfully",HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
 }
