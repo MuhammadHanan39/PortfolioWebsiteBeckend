48516d4303994a21477094b504039789c0e626b1
Hanan Shaikh <hannanshaikh150@gmail.com>
2024-03-26 06:31:32 +0500

    created the entities, controller and service classes

--- PortfolioBeckend/src/main/java/com/portfolio/serviceImpl/SendMailToOwnerImpl.java
+++ PortfolioBeckend/src/main/java/com/portfolio/serviceImpl/SendMailToOwnerImpl.java
@@ -1,5 +1,17 @@
 package com.portfolio.serviceImpl;
 
-public class SendMailToOwnerImpl {
+import org.springframework.stereotype.Service;
+
+import com.portfolio.entities.ContactMe;
+import com.portfolio.service.SendMailToOwner;
+
+@Service
+public class SendMailToOwnerImpl implements SendMailToOwner {
+
+	@Override
+	public boolean sendMail(ContactMe contactDetails) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
